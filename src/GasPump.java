58a2976a50c69d49f10627818aea3c385750b1b9
ColeAnderson7278 <canderson@basecampcodingacademy.org>
2019-01-24 11:00:43 -0600

    Added two methods to the GasPump class.
    The first method I added was selectPaymentType that allowed the user to
    choose what way they wanted to pay and then I'd assign their choice to
    a variable to use later. I also made a go method so that it is the only
    one public to the user. This will be were I run the other methods once
    I create them.

--- src/GasPump.java
+++ src/GasPump.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class GasPump {
+    private Double regularGasPrice = 1.89;
+    private Double premiumGasPrice = 1.97;
+    private Double supremeGasPrice = 2.06;
+    private Scanner user = new Scanner(System.in);
+    private String paymentType;
+
+    public void go(){
+        selectPaymentType();
+    }
+
+    private void selectPaymentType(){
+        System.out.println("Welcome to Gas-Mart! Please choose a payment option:\n1) Cash\n2) Card\n3) Cancel");
+        while(true){
+            String choice = user.nextLine();
+            if (choice.equals("1")){
+                System.out.println("You've chosen to pay with cash.");
+                paymentType = "cash";
+                break;
+            }else if (choice.equals("2")){
+                System.out.println("You've chosen to pay with a card.");
+                paymentType = "card";
+                break;
+            }else if (choice.equals("3")){
+                System.out.println("Goodbye! Hope to see you again soon.");
+                System.exit(0);
+            }System.out.println("Please enter a valid input.");
+        }
+    }
 }
--- src/Project.java
+++ src/Project.java
@@ -1,2 +1,6 @@
 public class Project {
+    public static void main(String[] args) {
+        GasPump gasPump = new GasPump();
+        gasPump.selectPaymentType();
+    }
 }
